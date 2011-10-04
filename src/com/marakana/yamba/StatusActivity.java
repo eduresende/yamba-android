3f2db628de24bcf028f101b289255c367414f98c
Eduardo Resende Pereira <ed.resende@gmail.com>
2011-10-04 20:34:12 -0300

    commit inicial

---
+++ src/com/marakana/yamba/StatusActivity.java
@@ -0,0 +1,120 @@
+package com.marakana.yamba;
+
+import winterwell.jtwitter.Twitter;
+import winterwell.jtwitter.TwitterException;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class StatusActivity extends Activity implements OnClickListener, TextWatcher {
+	
+	private static final String TAG = "StatusActivity";
+	EditText editText;
+	Button updateButton;
+	Twitter twitter;
+	TextView textCount;
+	
+    /** Called when the activity is first created. */
+    @SuppressWarnings("deprecation")
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.status);
+        
+        // Find Views
+        editText = (EditText) findViewById(R.id.editText);
+        
+        updateButton = (Button) findViewById(R.id.buttonUpdate);
+        updateButton.setOnClickListener(this);
+        
+        textCount = (TextView) findViewById(R.id.textCount);
+        textCount.setText(Integer.toString(140));
+        textCount.setTextColor(Color.GREEN);
+        editText.addTextChangedListener(this);
+        
+        twitter = new Twitter("student", "password");
+        twitter.setAPIRootUrl("http://yamba.marakana.com/api");
+    }
+    
+    // Asynchronously post to twitter
+    class PostToTwiter extends AsyncTask<String, Integer, String> {
+
+		@Override
+		protected String doInBackground(String... statuses) {
+			try {
+				Twitter.Status status = twitter.updateStatus(statuses[0]);
+				return status.text;
+			} catch (TwitterException e) {
+				Log.e(TAG, e.toString());
+				e.printStackTrace();
+				return "Failed to post";
+			}
+		}
+		
+		@Override
+		protected void onProgressUpdate(Integer... values) {
+			super.onProgressUpdate(values);
+			// Not used in this case
+		}
+		
+		// Called once the background activity has completed
+		@Override
+		protected void onPostExecute(String result) {
+			Toast.makeText(StatusActivity.this, result, Toast.LENGTH_LONG).show();
+		}
+		    	
+    }
+        
+    // Called when button is clicked
+    public void onClick(View v) {
+    	String status = editText.getText().toString();
+    	new PostToTwiter().execute(status);
+    	Log.d(TAG, "onClicked");
+    }
+    
+    // TextWatcher methods
+    public void afterTextChanged(Editable statusText) {
+    	int count = 140 - statusText.length();
+    	textCount.setText(Integer.toString(count));
+    	textCount.setTextColor(Color.GREEN);
+    	if (count < 10)
+    		textCount.setTextColor(Color.YELLOW);
+    	if (count < 0)
+    		textCount.setTextColor(Color.RED);
+    }
+    
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+    
+    public void onTextChanged(CharSequence s, int start, int before, int count){}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	MenuInflater inflater = getMenuInflater();
+    	inflater.inflate(R.menu.menu, menu);
+    	return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	switch (item.getItemId()) {
+    	case R.id.itemPrefs:
+    		startActivity(new Intent(this, PrefsActivity.class));
+    		break;
+    	}
+    	return true;
+    }
+}
